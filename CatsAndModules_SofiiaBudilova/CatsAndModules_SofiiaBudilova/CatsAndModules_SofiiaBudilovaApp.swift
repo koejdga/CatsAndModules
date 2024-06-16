c36c201201f20ccce83f5de50e33b6ff04e87942
Sofiia Budilova <108132458+koejdga@users.noreply.github.com>
2024-06-16 03:54:54 +0300

    Update CatsAndModules_SofiiaBudilovaApp.swift

--- CatsAndModules_SofiiaBudilova/CatsAndModules_SofiiaBudilova/CatsAndModules_SofiiaBudilovaApp.swift
+++ CatsAndModules_SofiiaBudilova/CatsAndModules_SofiiaBudilova/CatsAndModules_SofiiaBudilovaApp.swift
@@ -13,7 +13,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
     {
-        print("hello")
+        print("hi")
         FirebaseApp.configure()
         Crashlytics.crashlytics().setCustomValue(totalTappedRows, forKey: FirebaseKeys.TotalTappedRows.rawValue)
         loadCustomProperties()
